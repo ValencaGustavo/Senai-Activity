921ed50825b43ce952e3d75f4f8e0b8017088441
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2026-03-25 21:06:49 -0300

    Update Exercicio4.java

--- Exercicio4.java
+++ Exercicio4.java
@@ -32,6 +32,6 @@ public class Exercicio4 {
             System.out.print(noRepeticao[i] + " ");
         }
 
-        System.out.println("\nTotal: " + total);
+        System.out.println("Total: " + total);
     }
 }
